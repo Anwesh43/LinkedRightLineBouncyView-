7d9728ade9e368bc50f97bf3e8f1eaa8ad4611bf
Anwesh43 <anweshthecool0@gmail.com>
2019-12-09 02:14:23 +0530

    adding constants and libraries

--- rightlinebouncyview/src/main/java/com/anwesh/uiprojects/rightlinebouncyview/RightLineBouncyView.kt
+++ rightlinebouncyview/src/main/java/com/anwesh/uiprojects/rightlinebouncyview/RightLineBouncyView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.rightlinebouncyview
 /**
  * Created by anweshmishra on 09/12/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#0D47A1")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 30
+val rFactor : Float = 2.9f
