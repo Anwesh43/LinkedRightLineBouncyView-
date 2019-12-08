0f9935c2a3d939daefd1be4a61ea984676458c36
Anwesh43 <anweshthecool0@gmail.com>
2019-12-09 03:35:32 +0530

    adding fixes

--- rightlinebouncyview/src/main/java/com/anwesh/uiprojects/rightlinebouncyview/RightLineBouncyView.kt
+++ rightlinebouncyview/src/main/java/com/anwesh/uiprojects/rightlinebouncyview/RightLineBouncyView.kt
@@ -25,7 +25,7 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
-fun Float.cosify() : Float = 1f - Math.cos(this * Math.PI * 0.5f + Math.PI / 2).toFloat()
+fun Float.cosify() : Float = 1f - Math.sin(this * Math.PI * 0.5f + Math.PI / 2).toFloat()
 fun Float.fromTo(from : Float, to : Float) : Float = from + (to - from) * this
 
 fun Canvas.drawRightLineBouncy(scale : Float, size : Float, paint : Paint) {
