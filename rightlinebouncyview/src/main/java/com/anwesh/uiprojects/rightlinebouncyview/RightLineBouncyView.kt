3c38d4612d0ffccb2d4861f1a0c9085b9691cd43
Anwesh43 <anweshthecool0@gmail.com>
2019-12-09 02:44:39 +0530

    adding RightLineBouncy class

--- rightlinebouncyview/src/main/java/com/anwesh/uiprojects/rightlinebouncyview/RightLineBouncyView.kt
+++ rightlinebouncyview/src/main/java/com/anwesh/uiprojects/rightlinebouncyview/RightLineBouncyView.kt
@@ -159,4 +159,28 @@ class RightLineBouncyView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RightLineBouncy(var i : Int) {
+
+        private val root : RLBNode = RLBNode(0)
+        private var curr : RLBNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
